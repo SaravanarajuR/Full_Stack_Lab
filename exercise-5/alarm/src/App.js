6bb8b388dfeb5c5caeabd1ee9e2c0236d916dec4
Saravana raju R <88960950+SaravanarajuR@users.noreply.github.com>
2023-06-21 14:22:28 +0530

    Add files via upload

--- exercise-5/alarm/src/App.js
+++ exercise-5/alarm/src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import sound from "./alarm.mp3";
 import "./index.css";
 import Send from "./getAlarms";
-import { addAlarm, handleClose } from "./alarmFunctions";
+import { checkalarm, addAlarm, handleClose } from "./alarmFunctions";
 
 export default function App() {
   const audio = new Audio(sound);
@@ -16,43 +16,6 @@ export default function App() {
     return defaultAlarm;
   });
 
-  async function checkalarm(hrs, mins, day) {
-    if (alarm[`${day}`].includes(`${hrs}:${mins}`)) {
-      const secs = new Date().getSeconds();
-      if (secs === 0) {
-        document.getElementById("alarmWindow").style.visibility = "visible";
-        await audio.play();
-      }
-    }
-  }
-
-  function addAlarm(e) {
-    e.preventDefault();
-    let newAlarm = alarm;
-    const day = document.getElementById("day").value;
-    const inputTime = `${document.getElementById("hrs").value}:${
-      document.getElementById("mins").value
-    }`;
-    if (day === "8") {
-      for (let i of Object.keys(newAlarm)) {
-        if (newAlarm[`${i}`].includes(inputTime)) {
-          return null;
-        } else {
-          newAlarm[`${i}`] = [...newAlarm[`${i}`], inputTime];
-        }
-      }
-    } else {
-      if (newAlarm[`${day}`].includes(inputTime)) {
-        return null;
-      } else {
-        newAlarm = {
-          ...newAlarm,
-          [`${day}`]: [...newAlarm[`${day}`], inputTime],
-        };
-      }
-    }
-    setAlarm(newAlarm);
-  }
   useEffect(() => {
     const id = setInterval(() => {
       const date = new Date();
@@ -61,7 +24,7 @@ export default function App() {
       const secs = date.getSeconds();
       const day = date.getDay();
       setTime(`${hrs}-${mins}-${secs}`);
-      checkalarm(hrs, mins, day);
+      checkalarm(hrs, mins, day, alarm, audio);
     }, 1000);
     setId(id);
     return clearInterval(intervalId);
@@ -84,7 +47,7 @@ export default function App() {
         <input
           id="hrs"
           min="0"
-          max="12"
+          max="24"
           type="number"
           placeholder="hours"
           required
@@ -108,7 +71,12 @@ export default function App() {
           <option value={7}>Sunday</option>
           <option value={8}>Everyday</option>
         </select>
-        <button onSubmit={addAlarm} type="submit">
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            addAlarm(e, alarm, setAlarm);
+          }}
+        >
           Submit
         </button>
       </form>
--- exercise-5/alarm/src/alarmFunctions.js
+++ exercise-5/alarm/src/alarmFunctions.js
@@ -3,4 +3,41 @@ function handleClose(audio) {
   document.getElementById("alarmWindow").style.visibility = "hidden";
 }
 
-export { handleClose };
+function addAlarm(e, alarm, setAlarm) {
+  let newAlarm = alarm;
+  const day = document.getElementById("day").value;
+  const inputTime = `${document.getElementById("hrs").value}:${
+    document.getElementById("mins").value
+  }`;
+  if (day === "8") {
+    for (let i of Object.keys(newAlarm)) {
+      if (newAlarm[`${i}`].includes(inputTime)) {
+        return null;
+      } else {
+        newAlarm[`${i}`] = [...newAlarm[`${i}`], inputTime];
+      }
+    }
+  } else {
+    if (newAlarm[`${day}`].includes(inputTime)) {
+      return null;
+    } else {
+      newAlarm = {
+        ...newAlarm,
+        [`${day}`]: [...newAlarm[`${day}`], inputTime],
+      };
+    }
+  }
+  setAlarm(newAlarm);
+}
+
+async function checkalarm(hrs, mins, day, alarm, audio) {
+  if (alarm[`${day}`].includes(`${hrs}:${mins}`)) {
+    const secs = new Date().getSeconds();
+    if (secs === 0) {
+      document.getElementById("alarmWindow").style.visibility = "visible";
+      await audio.play();
+    }
+  }
+}
+
+export { checkalarm, addAlarm, handleClose };
